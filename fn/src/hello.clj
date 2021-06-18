5bb6e14bd02aed1a212028ac458199a5c48b4484
Douglas Naphas <douglasnaphas@gmail.com>
2021-06-18 16:59:19 -0400

    Add a POJO handler
    https://aws.amazon.com/blogs/compute/clojure/
    
    This adds the POJO handler from the blog post.

--- fn/src/hello.clj
+++ fn/src/hello.clj
@@ -4,3 +4,12 @@
 
 (defn -handler [s]
   (str "Hello " s "!"))
+
+; Add POJO handling
+
+(defn -handlepojo [this event]
+  (str "Hello " (.getFirstName event) " " (.getLastName event)))
+
+(gen-class
+  :name PojoHandler
+  :methods [[handlepojo [example.MyEvent] String]])
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -5,13 +5,23 @@ export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const fn = new lambda.Function(this, "Fn", {
+    const helloFn = new lambda.Function(this, "HelloFn", {
       runtime: lambda.Runtime.JAVA_11,
       handler: "hello::handler",
       code: lambda.Code.fromAsset(
         "../fn/target/lambda-clj-examples-0.1.0-standalone.jar"
       ),
       memorySize: 3000,
+      timeout: cdk.Duration.seconds(20),
+    });
+    const pojoFn = new lambda.Function(this, "PojoFn", {
+      runtime: lambda.Runtime.JAVA_11,
+      handler: "PojoHandler::handlepojo",
+      code: lambda.Code.fromAsset(
+        "../fn/target/lambda-clj-examples-0.1.0-standalone.jar"
+      ),
+      memorySize: 3000,
+      timeout: cdk.Duration.seconds(20),
     });
   }
 }
